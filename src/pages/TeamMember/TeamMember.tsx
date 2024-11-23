52cae0321b721bae06e14f597e8a8aa5afd2c762
kimgosung <rlarhtjd0123@gmail.com>
2024-11-24 00:54:22 +0900

    Feat: Add BottomSheet For Team Member

--- src/pages/TeamMember/TeamMember.tsx
+++ src/pages/TeamMember/TeamMember.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
+import React, {useCallback, useRef, useState} from 'react';
 import {
   View,
   Text,
   Pressable,
   StyleSheet,
   SafeAreaView,
-  Image,
 } from 'react-native';
 import IcLeftArrow from '../../assets/svg/ic_leftArrow.tsx';
 import useCustomNavigation from '../../hooks/useCustomNavigation.ts';
@@ -13,6 +12,12 @@ import IcMoreButton from '../../assets/svg/ic_moreButton.tsx';
 import {RouteProp, useRoute} from '@react-navigation/native';
 import {useSelector} from 'react-redux';
 import {RootState} from '../../app/store.ts';
+import BottomSheet, {BottomSheetBackdrop, BottomSheetView} from '@gorhom/bottom-sheet';
+import {
+  BottomSheetDefaultBackdropProps
+} from '@gorhom/bottom-sheet/lib/typescript/components/bottomSheetBackdrop/types';
+import EditIcon from '../../assets/svg/ic_editIcon.tsx';
+import TrashIcon from '../../assets/svg/ic_trash.tsx';
 
 type TeamMemberRouteProp = RouteProp<
   {ManageTeam: {teamId: string}},
@@ -20,15 +25,36 @@ type TeamMemberRouteProp = RouteProp<
 >;
 
 const TeamMember = () => {
+  const [isBottomSheetOpen, setIsBottomSheetOpen] = useState(false);
+
   const {navigation} = useCustomNavigation();
   const route = useRoute<TeamMemberRouteProp>();
   const {teamId} = route.params;
+
+  const bottomSheetRef = useRef<BottomSheet>(null);
 
   const team = useSelector((state: RootState) =>
     state.team.teams.find((t) => t.id === teamId)
   );
 
-  console.log(team);
+  const snapPoints = React.useMemo(() => ['18%'], []);
+
+  const handlePresentSheet = () => {
+    setIsBottomSheetOpen(true);
+    bottomSheetRef.current?.snapToIndex(0);
+  };
+
+  const renderBackdrop = useCallback(
+      (props: JSX.IntrinsicAttributes & BottomSheetDefaultBackdropProps) => (
+          <BottomSheetBackdrop
+              {...props}
+              disappearsOnIndex={-1}
+              appearsOnIndex={0}
+              pressBehavior="close"
+          />
+      ),
+      []
+  );
 
   return (
     <SafeAreaView style={styles.container}>
@@ -54,7 +80,9 @@ const TeamMember = () => {
           <Text style={{fontSize: 16, fontWeight: '700'}}>
             팀 멤버({team?.members.length})
           </Text>
-          <IcMoreButton color={'black'} style={styles.rotatedIcon} />
+          <Pressable onPress={handlePresentSheet}>
+            <IcMoreButton color={'black'} style={styles.rotatedIcon} />
+          </Pressable>
         </View>
         <View>
           <Text style={{fontWeight: '600', fontSize: 15}}>팀장</Text>
@@ -74,6 +102,29 @@ const TeamMember = () => {
           </View>
         </View>
       </View>
+      <BottomSheet
+          ref={bottomSheetRef}
+          index={isBottomSheetOpen ? 0 : -1}
+          backdropComponent={renderBackdrop}
+          containerStyle={{borderRadius: 18}}
+          snapPoints={snapPoints}
+          onChange={(index) => setIsBottomSheetOpen(index === 0)}
+          enablePanDownToClose
+          handleIndicatorStyle={{backgroundColor: '#d9d9d9', marginBottom: 8}}
+          style={styles.bottomSheet}>
+        <BottomSheetView style={styles.bottomSheetContent}>
+          <Pressable style={styles.bottomSheetItem}>
+            <EditIcon />
+            <Text style={styles.bottomSheetText}>팀장 변경하기</Text>
+          </Pressable>
+          <Pressable style={styles.bottomSheetItem}>
+            <TrashIcon />
+            <Text style={[styles.bottomSheetText, styles.deleteText]}>
+              팀원 관리하기
+            </Text>
+          </Pressable>
+        </BottomSheetView>
+      </BottomSheet>
     </SafeAreaView>
   );
 };
@@ -83,6 +134,38 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#FFFFFF',
     paddingHorizontal: 30,
+  },
+  moreOptionsButton: {
+    position: 'absolute',
+    top: 4,
+    right: 0,
+    zIndex: 1,
+    padding: 8,
+  },
+  bottomSheet: {
+    backgroundColor: 'white',
+    shadowColor: '#000',
+    borderRadius: 18,
+  },
+  bottomSheetContent: {
+    flex: 1,
+    paddingBottom: 16,
+    borderRadius: 18,
+    paddingHorizontal: 24,
+  },
+  bottomSheetItem: {
+    paddingVertical: 8,
+    marginVertical: 4,
+    alignItems: 'center',
+    flexDirection: 'row',
+    gap: 10,
+  },
+  bottomSheetText: {
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+  deleteText: {
+    color: 'red',
   },
   header: {
     flexDirection: 'row',
