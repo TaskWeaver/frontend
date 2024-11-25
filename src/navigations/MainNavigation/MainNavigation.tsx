19e1f2be0c33a36f8a6d4011b4dd46ffa1e670f2
김고성 <121794841+kimgosung@users.noreply.github.com>
2024-11-25 13:29:30 +0900

    [FEAT] SETTING PASSWORD (#24)
    * Feat: Find Password Page
    
    * Feat: Connect API
    
    * Feat: Setting Password

--- src/navigations/MainNavigation/MainNavigation.tsx
+++ src/navigations/MainNavigation/MainNavigation.tsx
@@ -9,54 +9,72 @@ import UserInformation from '../../pages/UserInformation/UserInformation.tsx';
 import EditTeamPage from '../../pages/EditTeam/EditTeam.tsx';
 import TeamMember from '../../pages/TeamMember/TeamMember.tsx';
 import CreateProject from '../../pages/CreateProject/CreateProject.tsx';
+import FindPassword from '../../pages/FindPassword/FindPassword.tsx';
+import FindPasswordAuth from '../../pages/FindPasswordAuth/FindPasswordAuth.tsx';
+import SetPassword from '../../pages/SetPassword/SetPassword.tsx';
 
 const Stack = createNativeStackNavigator<MainStackProps>();
 
 export default function MainNavigation() {
-  return (
-    <Stack.Navigator initialRouteName={'TopBarNavigation'}>
-      <Stack.Screen
-        name="TopBarNavigation"
-        component={TopBarNavigation}
-        options={{headerShown: false, gestureEnabled: false}}
-      />
-      <Stack.Screen
-        name="Notification"
-        component={NotificationContainer}
-        options={{headerShown: false}}
-      />
-      <Stack.Screen
-        name="CreateTeam"
-        component={CreateTeamContainer}
-        options={{headerShown: false}}
-      />
-      <Stack.Screen
-        name="ManageTeam"
-        component={ManageTeamContainer}
-        options={{headerShown: false}}
-      />
-      <Stack.Screen
-        name={'EditTeam'}
-        component={EditTeamPage}
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name={'EditUserInformation'}
-        component={UserInformation}
-        options={{headerShown: false}}
-      />
-      <Stack.Screen
-        name={'TeamMember'}
-        component={TeamMember}
-        options={{headerShown: false}}
-      />
-        <Stack.Screen
-            name={'CreateProject'}
-            component={CreateProject}
-            options={{headerShown: false}}
-        />
-    </Stack.Navigator>
-  );
+    return (
+        <Stack.Navigator initialRouteName={'TopBarNavigation'}>
+            <Stack.Screen
+                name="TopBarNavigation"
+                component={TopBarNavigation}
+                options={{headerShown: false, gestureEnabled: false}}
+            />
+            <Stack.Screen
+                name="Notification"
+                component={NotificationContainer}
+                options={{headerShown: false}}
+            />
+            <Stack.Screen
+                name="CreateTeam"
+                component={CreateTeamContainer}
+                options={{headerShown: false}}
+            />
+            <Stack.Screen
+                name="ManageTeam"
+                component={ManageTeamContainer}
+                options={{headerShown: false}}
+            />
+            <Stack.Screen
+                name={'EditTeam'}
+                component={EditTeamPage}
+                options={{
+                    headerShown: false,
+                }}
+            />
+            <Stack.Screen
+                name={'EditUserInformation'}
+                component={UserInformation}
+                options={{headerShown: false}}
+            />
+            <Stack.Screen
+                name={'TeamMember'}
+                component={TeamMember}
+                options={{headerShown: false}}
+            />
+            <Stack.Screen
+                name={'CreateProject'}
+                component={CreateProject}
+                options={{headerShown: false}}
+            />
+            <Stack.Screen
+                name={'FindPassword'}
+                component={FindPassword}
+                options={{headerShown: false}}
+            />
+            <Stack.Screen
+                name={'FindPasswordAuth'}
+                component={FindPasswordAuth}
+                options={{headerShown: false}}
+            />
+            <Stack.Screen
+                name={'SetPassword'}
+                component={SetPassword}
+                options={{headerShown: false}}
+            />
+        </Stack.Navigator>
+    );
 }
--- src/navigations/types.ts
+++ src/navigations/types.ts
@@ -1,26 +1,29 @@
 import {NavigatorScreenParams} from '@react-navigation/native';
 
 export type SignUpParamList = {
-  SignUp: undefined;
-  Authorization: {email: string; password: string; authCode: string};
-  Profile: {email: string; password: string};
-  Terms: {email: string; password: string; nickname: string; imageUrl: string};
+    SignUp: undefined;
+    Authorization: { email: string; password: string; authCode: string };
+    Profile: { email: string; password: string };
+    Terms: { email: string; password: string; nickname: string; imageUrl: string };
 };
 
 export type MainStackProps = {
-  TopBarNavigation: undefined;
-  Notification: undefined;
-  CreateTeam: undefined;
-  ManageTeam: {teamId: string};
-  EditUserInformation: {nickname: string; email: string; image: string};
-  EditTeam: {teamName: string; teamDescription: string; teamId: string};
-  TeamMember: {teamId: string};
-  CreateProject: {teamId: string};
+    TopBarNavigation: undefined;
+    Notification: undefined;
+    CreateTeam: undefined;
+    ManageTeam: { teamId: string };
+    EditUserInformation: { nickname: string; email: string; image: string };
+    EditTeam: { teamName: string; teamDescription: string; teamId: string };
+    TeamMember: { teamId: string };
+    CreateProject: { teamId: string };
+    FindPassword: undefined;
+    FindPasswordAuth: { email: string, authCode: string };
+    SetPassword: undefined;
 };
 
 export type RootStackParamList = {
-  OnBoarding: undefined;
-  LogIn: undefined;
-  SignUpStack: NavigatorScreenParams<SignUpParamList>;
-  MainStack: NavigatorScreenParams<MainStackProps>;
+    OnBoarding: undefined;
+    LogIn: undefined;
+    SignUpStack: NavigatorScreenParams<SignUpParamList>;
+    MainStack: NavigatorScreenParams<MainStackProps>;
 };
