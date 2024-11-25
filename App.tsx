323d0db7dfa6884a81917b4025bb25e1c5dbf95d
김고성 <121794841+kimgosung@users.noreply.github.com>
2024-11-25 19:42:03 +0900

    [Feat] Splash Screen iOS (#25)
    * Feat: Find Password Page
    
    * Feat: Connect API
    
    * Feat: Setting Password
    
    * Feat: Add Splash Image on iOS

--- App.tsx
+++ App.tsx
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {RootStackNavigator} from './src/navigations';
 import {Provider} from 'react-redux';
 import {store} from './src/app/store.ts';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import Toast from 'react-native-toast-message';
 import {PaperProvider} from 'react-native-paper';
+import SplashScreen from 'react-native-splash-screen';
 
 function App(): React.JSX.Element {
+
+    useEffect(() => {
+        setTimeout(() => {
+            SplashScreen.hide();
+        }, 1000);
+    });
+
     return (
         <GestureHandlerRootView style={{flex: 1}}>
             <Provider store={store}>
                 <PaperProvider>
-                    <RootStackNavigator />
+                    <RootStackNavigator/>
                 </PaperProvider>
             </Provider>
-            <Toast />
+            <Toast/>
         </GestureHandlerRootView>
     );
 }
